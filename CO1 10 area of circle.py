3b1e7eb2eab50401b0029d7076287952ee0df601
ANANDAKRISHNAN25 <118350840+ANANDAKRISHNAN25@users.noreply.github.com>
2022-11-30 02:35:16 -0800

    Delete CO1 10 area of circle.py

--- CO1 10 area of circle.py
+++
@@ -1,4 +0,0 @@
-import math as M  
-Radius = float (input ("Please enter the radius of the given circle: "))  
-area_of_circle = M.pi* Radius * Radius
-print (" The area of the given circle is: ", area_of_circle)  
