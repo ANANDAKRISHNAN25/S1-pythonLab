ce355f48b72acb3cde92ca9f44ad8d41e48d322f
ANANDAKRISHNAN25 <118350840+ANANDAKRISHNAN25@users.noreply.github.com>
2022-12-12 07:14:57 -0800

    co3

---
+++ CO3/CO3 1/CO3 1e.py
@@ -0,0 +1,8 @@
+import statistics as s
+list1=[10,20,30,40,50,60]
+print("mean :",s.mean(list1))
+print("median :",s.median(list1))
+print("mode :",s.mode(list1))
+print("Harmonic mean :",s.harmonic_mean(list1))
+print("varience :",s.variance(list1))
+print("statistics median low :",s.median_low([-12,6.6,-3,4,6,7.1,-9,22]))
