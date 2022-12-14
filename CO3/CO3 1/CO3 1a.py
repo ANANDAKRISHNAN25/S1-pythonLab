8315d100ea2242f0da630a978e2f6bc4528cb964
ANANDAKRISHNAN25 <sng22mca23@sngce.ac.in>
2022-12-14 00:36:58 -0800

    CO3 updated

---
+++ CO3/CO3 1/CO3 1a.py
@@ -0,0 +1,15 @@
+import random
+list1=['ann','ben','tom','jack']
+print(random.choice(list1))
+print(random.choices(list1,k=3))
+print(random.sample(list1,k=1))
+random.shuffle(list1)
+
+
+print(list1)
+print(random.randrange(3,9))
+
+
+
+
+      
