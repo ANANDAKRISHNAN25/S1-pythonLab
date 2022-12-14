bdb2a16f9ea1cb7d3b5087faea6ae05a239a51dd
ANANDAKRISHNAN25 <sng22mca23@sngce.ac.in>
2022-12-14 02:42:25 -0800

    del

--- CO3/new1.py
+++
@@ -1,8 +0,0 @@
-def greeting(name):
-    print("Hello" +name)
-    
-person1={
-    "name":"john",
-    "age":"26",
-    "country":"india"
-    }
--- CO3/new2.py
+++
@@ -1,4 +0,0 @@
-import new1
-#new1.greeting(" joseph")
-a=new1.person1["country"]
-print(a)
